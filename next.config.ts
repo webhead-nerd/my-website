8a25fbdc9e2fdffeeec79e075b17899890f2cae7
webhead-nerd <vishal.gunra@gmail.com>
2026-08-31 19:54:14 +0530

    fix: allow localimage optimization patterns for Next 16

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    localPatterns: [
+      {
+        pathname: "/images/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
